b5dd034f99aa8d3da6c108041de7fa8089a5d7a5
anzarshah <anzarshah43@gmail.com>
2020-10-19 13:11:00 +0530

    first comment

---
+++ precache-manifest.d1b535457d1cf604329db555cdb5306d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ea94e98528956c0510439aad9749ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023e1bf5a22f12366eb4",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "dd11781f1a0d69426bf6",
+    "url": "/static/js/2.cccd701c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.cccd701c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023e1bf5a22f12366eb4",
+    "url": "/static/js/main.bb020ee8.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
